6793f5057640e377a43fbd0f4f4a5996b9167fcc
Adao-Angelo <adaobegginer@gmail.com>
2025-01-04 20:20:55 +0100

    update project image

--- src/components/Project/ProjectRoot.tsx
+++ src/components/Project/ProjectRoot.tsx
@@ -9,7 +9,7 @@ export default function ProjectRoot({ children, dataAos }: ProjectRootProps) {
   return (
     <div
       data-aos={dataAos}
-      className="border border-neutral-200  dark:border-neutral-600 flex flex-col gap-[2.4rem] pb-[3.9rem]"
+      className="border border-neutral-200  rounded-[0.5rem] dark:border-neutral-600 flex flex-col gap-[2.4rem] pb-[3.9rem]"
     >
       {children}
     </div>
