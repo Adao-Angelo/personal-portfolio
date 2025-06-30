09be42a1161c4fc8cafb09d1669a38dfc483b9d0
Adao-Angelo <adaobegginer@gmail.com>
2025-06-30 12:27:51 +0100

    add duration

--- src/components/social/SocialSection.tsx
+++ src/components/social/SocialSection.tsx
@@ -3,7 +3,7 @@ import SocialCard from "./SocialCard";
 
 export default function SocialSection() {
   return (
-    <div className="max-w-[40rem] m-auto space-y-4">
+    <div className="max-w-[40rem] m-auto space-y-4 p-[1.8rem]">
       {socials.map((item) => (
         <SocialCard
           key={item.label}
