f0f5f41346bb933c5ec28bbd9326e5204ba6cc12
Adao-Angelo <adaobegginer@gmail.com>
2025-06-30 13:34:11 +0100

    update net

--- src/constants/socials.tsx
+++ src/constants/socials.tsx
@@ -8,22 +8,22 @@ import {
 export const socials = [
   {
     label: "LinkedIn",
-    href: "https://www.linkedin.com/in/seu-usuario",
+    href: "https://www.linkedin.com/in/adaoangelojoao/",
     icon: <LinkedinLogoIcon className="w-full h-full" />,
   },
   {
     label: "Instagram",
-    href: "https://www.instagram.com/seu-usuario",
+    href: "https://www.instagram.com/adao",
     icon: <InstagramLogoIcon className="w-full h-full" />,
   },
   {
     label: "GitHub",
-    href: "https://github.com/seu-usuario",
+    href: "https://github.com/adao-angelo",
     icon: <GithubLogoIcon className="w-full h-full" />,
   },
   {
     label: "Email",
-    href: "mailto:seuemail@exemplo.com",
+    href: "mailto:adaobegginerl@gmail.com",
     icon: <EnvelopeSimpleIcon className="w-full h-full" />,
   },
 ];
